009a01213987376c4b315d9ecc25a2470a9ccc88
danielmantillas <danielmantillas@gmail.com>
2026-04-23 21:35:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  transports: ['websocket', 'polling'],
-  cors: { origin: "*" }
-});
+const io = new Server(server);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ==================== ESTADO DEL JUEGO ====================
